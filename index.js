a72a05019aad830d61bbe1aad680891e5cdcd238
AJ <ajr.frontend@gmail.com>
2024-11-17 20:15:09 -0500

    Java additions not really
    struggling with java

---
+++ index.js
@@ -0,0 +1,10 @@
+//https://dog.ceo/api/breeds/image/random
+
+//async function main() {
+   // const dog = await fetch("https://dog.ceo/api/breeds/image/random")
+   // const dogData = await dog.jaon();
+   // console.log(dogData)
+//}
+//what am i even doing
+
+
